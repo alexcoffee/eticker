75acb31983cd5ff7ebd770f4e0f0ac74e63c8cc6
alexcoffee <poxit@hushmail.com>
2017-03-29 06:00:53 -0700

    display stock price

--- ticker.py
+++ ticker.py
@@ -1,87 +1,72 @@
 import sys
-import os
+import time
+
 from PIL import Image
 from PIL import ImageDraw
 from PIL import ImageFont
 from EPD import EPD
+from yahoo_finance import Share
 
 WHITE = 1
 BLACK = 0
-
-# fonts are in different places on Raspbian/Angstrom so search
-possible_fonts = [
-    '/usr/share/fonts/truetype/ttf-dejavu/DejaVuSansMono-Bold.ttf',   # R.Pi
-    '/usr/share/fonts/truetype/freefont/FreeMono.ttf',                # R.Pi
-    '/usr/share/fonts/truetype/LiberationMono-Bold.ttf',              # B.B
-    '/usr/share/fonts/truetype/DejaVuSansMono-Bold.ttf'               # B.B
-    '/usr/share/fonts/TTF/FreeMonoBold.ttf',                          # Arch
-    '/usr/share/fonts/TTF/DejaVuSans-Bold.ttf'                        # Arch
-]
-
-
-FONT_FILE = ''
-for f in possible_fonts:
-    if os.path.exists(f):
-        FONT_FILE = f
-        break
-
-if '' == FONT_FILE:
-    raise 'no font file found'
-
-FONT_SIZE = 40
-
+FONT_SIZE = 60
 MAX_START = 0xffff
-
-def main(argv):
-    """main program - draw and display a test image"""
-
-    try:
-        start = int(argv[0])
-    except ValueError:
-        sys.exit('start is not an integer: {s:s}'.format(s=argv[0]))
-    if start < 0 or start > MAX_START:
-        sys.exit('object count is out of range [0..0x{m:04x}: 0x{s:04x}'.format(m=MAX_START, s=start))
+FONT_FILE_SMALL = '/usr/share/fonts/truetype/roboto/Roboto-Thin.ttf'
+FONT_FILE = '/usr/share/fonts/truetype/roboto/Roboto-Bold.ttf'
 
 
+def main():
+    ticker = 'JNUG'
     epd = EPD()
-
-    print('panel = {p:s} {w:d} x {h:d}  version={v:s} COG={g:d} FILM={f:d}'.format(p=epd.panel, w=epd.width, h=epd.height, v=epd.version, g=epd.cog, f=epd.film))
-
     epd.clear()
 
-    demo(epd, start)
+    print('panel = {p:s} {w:d} x {h:d}  version={v:s} COG={g:d} FILM={f:d}'.format(p=epd.panel, w=epd.width, h=epd.height, v=epd.version, g=epd.cog, f=epd.film))
+    print_price(epd, ticker)
 
 
-def demo(epd, start):
-    """simple partial update demo - draw random shapes"""
-
-    # initially set all white background
+def print_price(epd, ticker):
     image = Image.new('1', epd.size, WHITE)
-
-    # prepare for drawing
     draw = ImageDraw.Draw(image)
     width, height = image.size
+    big_font = ImageFont.truetype(FONT_FILE, FONT_SIZE)
+    small_font = ImageFont.truetype(FONT_FILE_SMALL, 20)
 
-    font = ImageFont.truetype(FONT_FILE, FONT_SIZE)
+    price = Share(ticker)
 
-    counter = start & 0xffff
-
-    while True:
+    while 1:
         draw.rectangle((0, 0, width, height), fill=WHITE, outline=WHITE)
-        draw.text((0, 0), '{c:04X}'.format(c=counter), fill=BLACK, font=font)
-        counter = (counter + 1) & 0xffff
+        price.refresh()
 
-        # display image on the panel
+        # add vertical symbol name
+        txt_ticker = '{}'.format(ticker)
+        size_ticker = small_font.getsize(txt_ticker)
+        img_txt = Image.new('1', (height, size_ticker[1]), WHITE)
+        draw_txt = ImageDraw.Draw(img_txt)
+        draw_txt.text((height / 2 - (size_ticker[0] / 2), -4), txt_ticker, font=small_font)
+        img_txt = img_txt.rotate(90, expand=0)
+        image.paste(img_txt, (0, 0, size_ticker[1], height))
+
+        # print price
+        txt_price = '{}'.format(price.get_price())
+        size_price = big_font.getsize(txt_price)
+        draw.text((width / 2 - size_price[0] / 2 + size_ticker[1] / 2, -5), txt_price, fill=BLACK, font=big_font)
+
+        # print time
+        txt_time = '{}'.format(time.strftime("%H:%M:%S %p", time.localtime()))
+        size_time = small_font.getsize(txt_time)
+        draw.text((width - size_time[0] + 1, height - size_time[1] - 2), txt_time, fill=BLACK, font=small_font)
+
+        # divider
+        draw.line([size_ticker[1], 0, size_ticker[1], height])
+
         epd.display(image)
         epd.partial_update()
+        time.sleep(.5)
 
-# main
+
 if "__main__" == __name__:
-    if len(sys.argv) < 2:
-        sys.exit('usage: {p:s} start'.format(p=sys.argv[0]))
-
     try:
-        main(sys.argv[1:])
+        main()
     except KeyboardInterrupt:
         sys.exit('interrupted')
         pass
